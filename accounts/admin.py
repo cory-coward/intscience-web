a72e19029700e5559cd0df2f3d98ba26101387dc
cory-coward <cory.coward@sac-inc.com>
2022-11-04 07:52:52 -0500

    Add field for text alert address

--- accounts/admin.py
+++ accounts/admin.py
@@ -12,6 +12,13 @@ class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'first_name', 'last_name', 'email', 'receives_alert_emails', 'is_active', )
     list_per_page = 10
     list_editable = ('receives_alert_emails', 'is_active', )
+    fieldsets = (
+        ('Account and Personal Information',
+         {'fields': ('username', 'password', 'first_name', 'last_name', 'email', 'text_to_address',
+                     'receives_alert_emails', 'receives_text_alerts', )}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions', )}),
+        ('Account Login Information', {'fields': ('last_login', 'date_joined', )})
+    )
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -6,6 +6,13 @@ class CustomUser(AbstractUser):
     receives_alert_emails = models.BooleanField(default=False,
                                                 help_text='Indicates whether user should receive email notifications '
                                                           'of alarms')
+    text_to_address = models.CharField(max_length=50,
+                                       blank=True,
+                                       help_text='Example: 5554441111@txt.att.net (Consult online for carrier-specific '
+                                                 'information)')
+    receives_text_alerts = models.BooleanField(default=False,
+                                               help_text='Indicates whether user should receive text notifications '
+                                                         'of alarms')
 
     def __str__(self):
         return f'{self.first_name} {self.last_name} ({self.username})'
